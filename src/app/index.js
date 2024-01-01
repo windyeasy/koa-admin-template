3f8b1b62527e2a3d53d7936bb2ca2fabe541e67a
windyeasy <windyeasy13gb@163.com>
2024-01-01 19:28:48 +0800

    feat: 添加跨域功能

--- src/app/index.js
+++ src/app/index.js
@@ -2,8 +2,10 @@ const Koa = require("koa");
 const app = new Koa();
 const bodyParser = require("koa-bodyparser");
 const registerRouter = require("../router");
-
+const cors = require("@koa/cors");
 app.use(bodyParser());
+// 添加全局跨域
+app.use(cors());
 
 // 注册路由
 registerRouter(app);
