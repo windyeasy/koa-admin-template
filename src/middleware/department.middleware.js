05599f19a871bacf1fcff2c2bd9347a854f6fa88
windyeasy <windyeasy13gb@163.com>
2024-01-04 15:14:37 +0800

    fix: 修复部门编辑bug

--- src/middleware/department.middleware.js
+++ src/middleware/department.middleware.js
@@ -7,6 +7,7 @@ const {
 const departmentService = require("../services/department.service");
 const { checkValueNotDefined } = require("../utils/checkValue");
 const { fetchId } = require("../utils/fetch-id");
+const fetchParamsId = require("../utils/fetch-params-id");
 
 async function verifyDepartment(ctx, next) {
   const {
@@ -48,6 +49,48 @@ async function verifyDepartment(ctx, next) {
   };
   await next();
 }
+async function verifyEditDepartment(ctx, next) {
+  const {
+    depName,
+    sort = 0,
+    state = 1,
+    intro = "",
+    parentId = null,
+  } = ctx.request.body;
+  if (!depName) {
+    return errorRequest.throw(FIELD_NOT_NULL, ctx, "部门名称");
+  }
+  //   判断是否顶层菜单, 顶层部门名不能重复
+  if (parentId === null || parentId === "") {
+    const depList = await departmentService.queryDataByKeyValue({
+      key: "id",
+      value: fetchParamsId(ctx),
+    });
+    // 判断查询出来的名称是否是当前编辑的名称
+    if (depList && depList.length && depList[0].depName != depName) {
+      return errorRequest.throw(IS_EXISTS, ctx, "部门名称");
+    }
+  }
+  //   验证上级部门是否存在
+  if (checkValueNotDefined(parentId) && parentId !== "") {
+    const depList = await departmentService.queryDataByKeyValue({
+      key: "id",
+      value: parentId,
+    });
+    if (depList && !depList.length) {
+      return errorRequest.throw(IS_NOT_EXISTS, ctx, "上级部门");
+    }
+  }
+  ctx.payload = {
+    depName,
+    sort,
+    state,
+    intro,
+    parentId: fetchId(parentId),
+  };
+  await next();
+}
 module.exports = {
   verifyDepartment,
+  verifyEditDepartment,
 };
--- src/router/department.router.js
+++ src/router/department.router.js
@@ -8,7 +8,10 @@ const {
   detail,
   allList,
 } = require("../controller/department.controller");
-const { verifyDepartment } = require("../middleware/department.middleware");
+const {
+  verifyDepartment,
+  verifyEditDepartment,
+} = require("../middleware/department.middleware");
 
 const departmentRouter = new KoaRouter({
   prefix: "/department",
@@ -18,12 +21,12 @@ departmentRouter.post("/", verifyAuth, verifyDepartment, create);
 // 删除部门
 departmentRouter.delete("/:id", verifyAuth, remove);
 // 编辑部门
-departmentRouter.patch("/:id", verifyAuth, verifyDepartment, update);
+departmentRouter.patch("/:id", verifyAuth, verifyEditDepartment, update);
 // 获取部门列表
 departmentRouter.get("/", verifyAuth, list);
 // 查询未禁用全部部门列表
 departmentRouter.get("/all", verifyAuth, allList);
 // 获取部门详情
 departmentRouter.get("/:id", verifyAuth, detail);
-// departmentRouter.get("/:r")
+
 module.exports = departmentRouter;
